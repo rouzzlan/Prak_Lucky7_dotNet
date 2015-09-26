f54b179f188e7d09ab1dfd5593d2d0b6e64eab4e
rouslankhayaouri <rouslan_kh@hotmail.com>
2015-09-26 11:15:31 +0200

    first commit dotnet exercise

---
+++ Lucky7/Lucky7.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucky7
+{
+  public partial class Lucky7
+  {
+    private Random generateRandomNumber = new Random();
+    private int gekozenGetal = 7;
+
+    public Lucky7()
+    {
+
+    }
+
+    public Lucky7(int gekozenGetal)
+    {
+      this.gekozenGetal = gekozenGetal;
+    }
+
+    public bool Try(out int getal1, out int getal2, out int getal3)
+    {
+      getal1 = generateRandomNumber.Next(1, 9);
+      getal2 = generateRandomNumber.Next(1, 9);
+      getal3 = generateRandomNumber.Next(1, 9);
+      if (getal1 == gekozenGetal || getal2 == gekozenGetal || getal3 == gekozenGetal)
+      {
+        return true;
+      }
+      return false;
+    }
+  }
+}
---
+++ Lucky7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucky7
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      bool doorgaan = true;
+      bool gewonnen = false;
+      string proceedY_N = "";
+      Lucky7 lucky7 = new Lucky7();
+      int getal1, getal2, getal3;
+
+      while (doorgaan)
+      {
+        gewonnen = lucky7.Try(out getal1, out getal2, out getal3);
+        Console.WriteLine("{0}\t{1}\t{2}", getal1, getal2, getal3);
+        if (gewonnen)
+        {
+          Console.WriteLine("Gewonnen");
+        }
+        else
+        {
+          Console.WriteLine("Verloren");
+        }
+        Console.WriteLine("Another try <y/n>");
+        proceedY_N = Console.ReadLine();
+        if (proceedY_N.ToUpper().Equals("N"))
+        {
+          doorgaan = false;
+        }
+      }
+
+    }
+  }
+}
